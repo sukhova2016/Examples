c2638d9ef65ebb94be6a546138dff40e422d186d
Polina Drokina <sukhova2016@inbox.ru>
2022-10-30 09:43:21 +0400

    Example008_seminar

---
+++ Example008_seminar/Program.cs
@@ -0,0 +1,14 @@
+﻿int sum = 0;
+int startNumber = 1;
+
+while (startNumber <= 15)
+{
+    if (startNumber % 2 == 0)
+    {
+        sum = sum + startNumber;
+    }
+    startNumber += 1;
+
+}
+
+Console.WriteLine(sum);
